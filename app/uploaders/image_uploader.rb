aa7073cb55bcbcd04ef30f50487f118ca5476f64
takahata-noria <takahatanoria@gmail.com>
2019-11-30 09:14:37 +0900

    画像ファイルサイズ調整

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [600, 600]
+  process resize_to_fit: [250, 250]
     # サムネイルを生成する設定
   version :thumb do
     process resize_to_fit: [100, 100]
