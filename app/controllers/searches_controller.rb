33738794308a71d9f9586754805afa4ba7625669
takahata-noria <takahatanoria@gmail.com>
2019-12-01 04:51:44 +0900

    デプロイエラー修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,13 @@
-class SearchesController < ApplicationController
-  include CommonActions
-  # before_action :set_parents #set_parentsはcontoroller/concerns/common_action.rbに記述
-  def index
-    # @products     = Product.where('name LIKE(?)', "%#{params[:keyword]}%").order("created_at DESC") unless params[:keyword].empty?
-    # @new_products = Product.order("created_at DESC") unless @products.present?
-    # @keyword   = params[:keyword]
+# class SearchesController < ApplicationController
+#   include CommonActions
+#   # before_action :set_parents #set_parentsはcontoroller/concerns/common_action.rbに記述
+#   def index
+#     # @products     = Product.where('name LIKE(?)', "%#{params[:keyword]}%").order("created_at DESC") unless params[:keyword].empty?
+#     # @new_products = Product.order("created_at DESC") unless @products.present?
+#     # @keyword   = params[:keyword]
 
-    @article = Article.where('titile LIKE(?)',"%#{params[:keyword]}%".order("created_at DESC") unless params[:keyword].empty?
-    @new_article = Article.order("created_at DESC") unless @article .present?
-    @keyword   = params[:keyword]
-  end
-end
+#     @article = Article.where('titile LIKE(?)',"%#{params[:keyword]}%".order("created_at DESC") unless params[:keyword].empty?
+#     @new_article = Article.order("created_at DESC") unless @article .present?
+#     @keyword   = params[:keyword]
+#   end
+# end
