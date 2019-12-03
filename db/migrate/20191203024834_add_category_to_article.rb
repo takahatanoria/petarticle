5250fc8f94ab3a5449efd2fb5ada63a1f54c19bf
takahata-noria <takahatanoria@gmail.com>
2019-12-03 19:01:55 +0900

    デプロイエラー修正

--- db/migrate/20191203024834_add_category_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToArticle < ActiveRecord::Migration[5.2]
-  def change
-    add_column :articles, :category_id, :integer
-  end
-end
--- db/migrate/20191203093650_change_column_to_not_null_category.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToNotNullCategory < ActiveRecord::Migration[5.2]
-  def up
-    change_column :articles, :category_id,:integer, null: false
-  end
-
-  def down
-    change_column :articles, :category_id,:integer
-  end
-end
