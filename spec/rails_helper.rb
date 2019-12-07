4d5554c8ad619bfab85391eec03b5a31201a688f
takahata-noria <takahatanoria@gmail.com>
2019-12-08 05:16:24 +0900

    単体テストarticlesmodel、controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,13 +33,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-  # config.include Devise::Test::ControllerHelpers, type: :controller
-  # config.include ControllerMacros, type: :controller
-  # config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include FactoryBot::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
