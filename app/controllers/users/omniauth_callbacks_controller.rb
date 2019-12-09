29c04c5fedd42332c71995bf542303f65fc64be3
takahata-noria <takahatanoria@gmail.com>
2019-12-09 10:53:06 +0900

    SNS認証　ローカルで完了

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+
+  def facebook
+    callback_from :facebook
+  end
+
+  def google_oauth2
+    callback_from :google
+  end
+
+  private
+
+  def callback_from(provider)
+    provider = provider.to_s
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session[:name] = @user.name
+      session[:email] = @user.email
+      session[:password] = @user.password
+      session[:provider] = @user.provider
+      session[:uid] = @user.uid
+
+      redirect_to new_user_registration_sns_path
+    end
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,38 @@ class User < ApplicationRecord
   has_many :articles  
   has_many :comments  
   has_one :profile
-  
+
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+
+
+         def self.find_for_oauth(auth)
+          user = User.where(uid: auth.uid, provider: auth.provider).first
+      
+          unless user
+        
+            user = User.create(
+              uid:      auth.uid,
+              provider: auth.provider,
+              name:     auth.info.name,
+              email:    User.dummy_email(auth),
+              password: Devise.friendly_token[0, 20],
+            
+            )
+          end
+      
+          user
+        end
+      
+        private
+      
+        def self.dummy_email(auth)
+          "#{auth.uid}-#{auth.provider}@example.com"
+        end
+
+
   # accepts_nested_attributes_for :profile, allow_destroy: true
 
   # has_one_attached :avatar
